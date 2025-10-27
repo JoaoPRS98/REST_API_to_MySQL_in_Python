dae5618562b408225e9898ca5e217aa4506f201b
João Santos <santos_paulo17@hotmail.com>
2025-10-27 20:47:03 +0000

    Delete transform.py

--- transform.py
+++
@@ -1,8 +0,0 @@
-from extract import extract_api_data
-import pandas as pd
-
-json_data = extract_api_data()
-
-products = json_data["data"]["products"]
-df = pd.json_normalize(products)
-print(df.head())
